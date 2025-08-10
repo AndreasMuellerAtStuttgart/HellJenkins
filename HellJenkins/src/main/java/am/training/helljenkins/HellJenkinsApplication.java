fd34b9c7afb99cff9ee97beaa155acc17bf6fc27
Andreas Müller <andreasmuller@Mac.fritz.box>
2025-08-10 16:27:59 +0200

    initial commit

---
+++ HellJenkins/src/main/java/am/training/helljenkins/HellJenkinsApplication.java
@@ -0,0 +1,13 @@
+package am.training.helljenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellJenkinsApplication.class, args);
+    }
+
+}
---
+++ HellJenkins/src/test/java/am/training/helljenkins/HellJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package am.training.helljenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
